65b0ba84f7c4e38e08a5fec4dda7bac8f5b06867
dhentosz <dmhentosz@gmail.com>
2024-10-28 10:43:42 -0400

    Added theme toggle button with basic functionality.
    App still pulls user system preference initially to set theme dynamically and then toggles light and dark mode upon first click of "Theme" button.

--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useRef } from "react";
 import styles from "./Header.module.css";
 
 export default function Header() {
+  // initiate reference to button element
+  const btnRef = useRef<HTMLButtonElement>(null!);
+
+  useEffect(() => {
+    const btn = btnRef.current; // pull current button element
+    let darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches; // checks if user system preference is dark and stores boolean.
+
+    // listen for click on btn and toggle light/dark mode based on current state.
+    btn.addEventListener("click", () => {
+      if (darkMode) {
+        document.documentElement.style.setProperty("color-scheme", "light");
+        darkMode = !darkMode;
+      } else {
+        document.documentElement.style.setProperty("color-scheme", "dark");
+        darkMode = !darkMode;
+      }
+    });
+  });
   return (
     <>
       <div className={styles.header}>
         <img src="src/assets/logo.png" alt="LOGO" id={styles.logo}></img>
         ResCalc
+        <button ref={btnRef}>Theme</button>
       </div>
     </>
   );

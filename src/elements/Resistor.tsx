d985c797453834c08e0f4059f1e3e3364dfc630e
dhentosz <dmhentosz@gmail.com>
2024-11-09 15:22:35 -0500

    Added more comments to CalculateOutput()

--- src/elements/Resistor.tsx
+++ src/elements/Resistor.tsx
@@ -1,4 +1,4 @@
-// Main resistor component/UI. Displays color bands for users to interact with in order to choose band colors to calculate ohm rating. Displays Output component and passes necessary data to it.
+// Main resistor component/UI. Displays color bands for users to interact with in order to choose band colors to calculate ohm rating. Displays Output component and passes necessary data to it dependant on state.
 
 import styles from "./Resistor.module.css";
 import ColorBand from "./ColorBand";
@@ -6,26 +6,29 @@ import Output from "./Output.tsx";
 import { useState } from "react";
 
 export default function Resistor() {
-  // Defines necessary state values for calculating ohm rating dependant on current color band colors
+  // Defines necessary state values for calculating ohm rating dependant on current color band selections
   const [firstValue, setFirstValue] = useState(0);
   const [secValue, setSecValue] = useState(0);
   const [multiplier, setMultiplier] = useState(0);
   const [tolerance, setTolerance] = useState(0);
 
-  // Calculates the final output to be displayed.
+  // Calculates the final output to be displayed
   function CalculateOutput(
     fVal: number,
     sVal: number,
     mVal: number,
     tVal: number
   ) {
+    // Creates one single value out of first and second band values as base to be multiplied
     let basevalue = 10 * fVal + sVal;
+    // Multiplies baseValue by the 3rd bands value (multiplier) for final ohm rating
     let ohmRating = basevalue * mVal;
 
+    // Returns a string with formatted output including ohm rating and tolerance percentage
     return ohmRating + "ohms " + tVal + "% tol";
   }
 
-  // Calls CalculateOutput with state variables and stores into output variable.
+  // Calls CalculateOutput with state variables and stores into output variable
   let output = CalculateOutput(firstValue, secValue, multiplier, tolerance);
 
   return (
